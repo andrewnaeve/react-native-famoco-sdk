b84f9e5ad3dbed3854c16827f2f60412dd7ab956
Andrew N <andrew@gingerbreadshed.com>
2018-08-21 16:04:16 -0700

    basic java setup

---
+++ famoko-sdk/src/main/java/com/gingerbreadshed/famokosdk/App.java
@@ -0,0 +1,13 @@
+package com.gingerbreadshed.famokosdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
