b28859b3eac6ce7178619db7a72a0463b982840d
Andrew N <andrew@gingerbreadshed.com>
2018-08-21 16:47:51 -0700

    add Famoko class

---
+++ android/src/main/java/com/reactlibrary/Famoko.java
@@ -0,0 +1,18 @@
+package com.FamokoSdk;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Famoko extends ReactContextBaseJavaModule {
+
+  public Famoko(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+  
+}
