2d6bb8c4007840d846f43154d777d37aee27c644
Andrew N <andrew@gingerbreadshed.com>
2018-08-23 12:03:02 -0700

    add os packages

--- android/src/main/java/com/famokosdk/RNFamokoSdkModule.java
+++ android/src/main/java/com/famokosdk/RNFamokoSdkModule.java
@@ -1,5 +1,9 @@
 package com.reactlibrary;
 
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
